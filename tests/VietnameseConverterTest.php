f5de3c1a75276a5a0ee3a00f883110d930f51a14
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-09-04 23:46:03 +0700

    init composer package, add basic test

---
+++ tests/VietnameseConverterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kenny\VietnameseConverter\Tests;
+
+use Kenny\VietnameseConverter\VnCharsetConversion;
+use PHPUnit\Framework\TestCase;
+
+class VietnameseConverterTest extends TestCase
+{
+    /** @test */
+    public function it_can_created()
+    {
+        $vniConversion = new VnCharsetConversion();
+
+        $this->assertNotNull($vniConversion);
+    }
+
+    /** @test */
+    public function it_can_converted_from_unicode_to_ascii()
+    {
+        $vniConversion = new VnCharsetConversion();
+
+        $output = $vniConversion->convert('Tôi tên là Ngô Chí Dũng. Người đã tạo ra cái phần mềm này.');
+
+        $this->assertNotEmpty($output);
+        $this->assertSame('Toi ten la Ngo Chi Dung. Nguoi da tao ra cai phan mem nay.', $output);
+    }
+
+    /** @test */
+    public function it_can_converted_from_unicode_to_virq()
+    {
+        $vniConversion = new VnCharsetConversion();
+
+        $output = $vniConversion->convert('Tôi tên là Ngô Chí Dũng. Người đã tạo ra cái phần mềm này.', 'unicode', 'virq');
+        $this->assertNotEmpty($output);
+
+        $this->assertEquals("To^i te^n la` Ngo^ Chi' Du~ng. Ngu+o+`i d-a~ ta.o ra ca'i pha^`n me^`m na`y.", $output);
+    }
+}
